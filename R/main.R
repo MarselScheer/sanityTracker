fef35143c1164e4be0cd8b8382e1cd65316b60d0
m <scheer@freescience.de>
2020-03-26 17:29:37 +0100

    init. basic functionality for adding sanity checks

---
+++ R/main.R
@@ -0,0 +1,64 @@
+TRACKER_ENV <- new.env()
+
+#' Adds a sanity check to the list of already performed sanity checks
+#'
+#' @param fail_vec logical vector where TRUE indicates that a fail has happend
+#' @param description of the sanity check
+#' @param counter_meas description of the counter measures that were applied
+#'   to correct the problems
+#' @param data optional. Data set where the fails were found. Is used to store examples
+#'   of failures
+#' @param example_size number failures to be extracted from the object passed to the parameter
+#'   data. By default 3 random examples are extracted.
+#'
+#' @return invisibly the sanity check that is stored internally with the other sanity checks.
+#'   All performed sanity checks can be fetched via \link{get_sanity_checks}
+#' @export
+#'
+#' @examples
+#' d <- data.frame(person_id = 1:4, bmi = c(18,23,-1,35), age = 31:34)
+#' add_sanity_check(d$bmi < 15, description = "bmi too low", counter_meas = "none",
+#'   data = d)
+#' add_sanity_check(d$bmi > 30, description = "bmi too high", counter_meas = "none")
+#' get_sanity_checks()
+add_sanity_check <- function(
+  fail_vec, description, counter_meas, data, example_size = 3) {
+
+  row <- data.table::data.table(
+    description = description,
+    n = length(fail_vec),
+    n_fail = sum(fail_vec, na.rm = TRUE),
+    n_na = sum(is.na(fail_vec)),
+    counter_meas = counter_meas
+    )
+
+  if (!missing(data) & any(fail_vec)) {
+    # add some examples where the fail occured
+
+    idx <- which(fail_vec)
+    if (length(idx) == 1) {
+      fail_example <- idx
+    } else {
+      fail_example <- sample(which(fail_vec), size = min(example_size, row$n_fail))
+    }
+
+    row$example <- list(data[fail_example,])
+  }
+
+  TRACKER_ENV[["checks"]] <- data.table::rbindlist(
+    list(TRACKER_ENV[["checks"]], row),
+    use.names = TRUE,
+    fill = TRUE
+  )
+  return(invisible(row))
+}
+
+#' Returns all performed sanity checks
+#'
+#' @return all sanity checks
+#' @seealso \link{add_sanity_check}
+#' @export
+get_sanity_checks <- function() {
+  return(TRACKER_ENV[["checks"]])
+}
+
