368598a682ae26380f47f75ecedb27adba7bf5ef
Marsel Scheer <scheer@freescience.de>
2021-09-01 22:41:44 +0200

    add docker from Rpkgtemplate

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-
-sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v ~/docker_fs:/home/rstudio sanitytracker_ide
+sudo docker run --rm -d -p 8787:8787 -e DISABLE_AUTH=true \
+  -v ~/docker_fs:/tmp/hostfs \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  rstudio_emacs:4.1.0
